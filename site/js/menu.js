ba6142a4969d5b302dc483a2bfd97ee3fb186953
Guilherme Garrote <guilhermegg2008@gmail.com>
2023-09-02 11:30:09 -0300

    Header
    Header responsivo feito.

---
+++ site/js/menu.js
@@ -0,0 +1,29 @@
+var header = document.getElementById('header');
+var navigationHader = document.getElementById('navigationHader');
+var content = document.getElementById('content');
+var showSidebar = false;
+
+function toggleSidebar() {
+    showSidebar = !showSidebar;
+    if (showSidebar) {
+        navigationHader.style.marginLeft = '-10vh';
+        navigationHader.style.animationName = 'showSidebar';
+        content.style.filter = 'blur(4px)';
+    } else {
+        navigationHader.style.marginLeft = '-100vh';
+        navigationHader.style.animationName = '';
+        content.style.filter = 'blur(0px)';
+    }
+}
+
+function closeSidebar(){
+    if(showSidebar){
+        toggleSidebar();
+    }
+}
+
+window.addEventListener('resize', function(event){
+    if(this.window.innerWidth > 820 && showSidebar){
+        toggleSidebar();
+    }
+});
